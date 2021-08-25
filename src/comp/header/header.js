d6611702f1b5173dfa49ede11fe46bc84758cb26
Andras <andrisgombos@gmail.com>
2021-08-25 17:01:20 +0200

    intital setup

---
+++ src/comp/header/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HeaderContainer } from './style';
+
+
+const Header = () => {
+
+    return (
+
+        <HeaderContainer>
+            <button>Home</button>
+            <button>Search</button>
+            <button>Login</button>
+        </HeaderContainer>
+
+    )
+
+}
+
+export default Header;
---
+++ src/comp/header/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    top: 0;
+    background-color: lightblue;
+    `
