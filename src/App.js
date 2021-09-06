b5e84b77da7d9cd60fe88b536983a0e646eb9c08
Andras <andrisgombos@gmail.com>
2021-09-06 10:24:51 +0200

    routes setup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import AddRecipe from './comp/addrecipe/add';
 import RecipeIngridients from './comp/addrecipe/ingridient';
 import Header from './comp/header/header';
 import HomePage from './comp/home/startingpage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      {/* <HomePage/> */}
-      <AddRecipe/>
-      {/* <RecipeIngridients/> */}
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/add' component={AddRecipe}/>
+          <Route exact path='/header' component={Header}/>
+        </Switch>
+      </Router>
     </>
   );
 }
